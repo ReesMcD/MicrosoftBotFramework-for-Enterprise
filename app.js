b85c8311593c9d46a047c6eb670476d94333d52f
[Rees] <[mcdevittrees@gmail.com]>
2017-07-05 20:05:55 -0500

    Parses json for title and path

--- app.js
+++ app.js
@@ -37,11 +37,34 @@ bot.dialog('help', (session, args, next) => {
 });
 
 bot.dialog('search', (session, args, next) => {
-  request('https://jsonplaceholder.typicode.com/posts/1', function(err, res, body) {
+  request('http://127.0.0.1:8080/', function(err, res, body) { // TODO: Host here is local host to for json file --> change to proxy server
+    console.log(res.statusCode);
     json = JSON.parse(body)
-    console.log(json);
-    output = json.id //Change to needed data
-    session.endDialog(output.toString());
+    var results = []
+    var resultArray = json['d:query']['d:PrimaryQueryResult']['d:RelevantResults']
+      ['d:Table']['d:Rows']['d:element']
+
+    for (i = 0; i < resultArray.length; i++) {
+      var rowArray = resultArray[i]['d:Cells']['d:element']
+
+      for (j = 1; j < rowArray.length; j++) {
+        var title, path, result;
+
+        if (rowArray[j]['d:Key'] == 'Title' && rowArray[j + 1]['d:Key']) {
+          title = rowArray[j]['d:Value'].toString()
+          path = rowArray[j + 1]['d:Value'].toString()
+
+          result = {
+            'title': title,
+            'path': path
+          }
+          results.push(result);
+
+        }
+      }
+    }
+    console.log(results);
+    //No message sent
   })
 }).triggerAction({
   matches: /^search$/,
